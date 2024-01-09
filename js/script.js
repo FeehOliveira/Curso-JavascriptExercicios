037c54f6620b3b361ee3ee5f7480315c8cf45ed0
Felipe Oliveira <feeholiveira10@outlook.com>
2024-01-09 05:44:14 -0300

    finalizando fundamentos

--- js/script.js
+++ js/script.js
@@ -80,15 +80,111 @@ switch(fruta) {
 
 //Estrutura de Repetição
 
+/*
 for(let i = 0; i < 5; i = i++) {
     console.log("O Valor de i é: " +i);
 }
+*/
+
 
 // while  = necessário declarar o valor inicial 
 
 let k = 0;
 
-while(i < 5) {
+while(k < 5) {
     console.log("O valor de k: " + k )
-    i++;
+    k++;
 }
+
+// Funções
+
+function cumprimentar(nome) {
+    console.log("Olá, " + nome)
+}
+
+cumprimentar("Felipe")
+
+// Escopo de variaveis
+
+let cor = "Azul"
+
+function mostrarCor() {
+    let cor = "verde";
+
+    console.log(cor)
+}
+
+mostrarCor();
+
+// hoisting = içamento
+
+testeHoisting();
+
+function testeHoisting() {
+    console.log("Deu Certo!")
+}
+
+// arrow function
+
+const testeArrow = () => { console.log("Function Arrow")};
+
+testeArrow();
+
+// trusthy e falsy
+
+const minhaVariavel1 = " " //falsy
+const minhaVariave2 = "Texto" //
+
+if(minhaVariave2) {
+    console.log("Verdadeiro")
+}else {
+    console.log("Falso")
+}
+
+// array, listas
+
+const numeros = [1,2,3,4,5]
+
+console.log(numeros)
+
+numeros.push(6);
+
+console.log(numeros)
+
+numeros.pop(6)
+
+console.log(numeros)
+
+// strings
+
+const minhaString = "Olá,"
+
+const minhaString2 = minhaString + "Tudo bem?"
+
+console.log(minhaString2)
+
+//interpolação
+
+let nome5 = "Felipe"
+
+const minhaString4 = `Olá ${nome5}, como você está`;
+
+console.log(minhaString4)
+console.log(minhaString4.length);
+console.log(minhaString4[5])
+console.log(minhaString4.toUpperCase())
+
+// Data e Hora
+
+const agora = Date();
+console.log(agora);
+
+const natal = new Date(2023, 11, 25)
+
+console.log(natal)
+
+// Math
+
+console.log(Math.PI);
+console.log(Math.sqrt(16));
+console.log(Math.pow(2, 3));
